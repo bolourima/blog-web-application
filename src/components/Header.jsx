a4098d052a8391f45b06ba02f242cae1f4466ba6
Bolormaa <135795277+bolourima@users.noreply.github.com>
2023-12-21 19:19:45 +0800

    BlogPost

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MetaBlogLogo } from "./Svg images/MetaBlogLogo";
+
+export const Header = () => {
+  return (
+    <div>
+      <MetaBlogLogo />
+    </div>
+  );
+};
