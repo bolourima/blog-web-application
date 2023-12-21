7814286ca80e89186a5f781ae47a11308142cfa1
Bolormaa <135795277+bolourima@users.noreply.github.com>
2023-12-21 19:32:08 +0800

    trending

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -9,51 +9,53 @@ export const BlogPosts = () => {
       .then((response) => response.json())
       .then((data) => setArticles(data));
   }, []);
-  console.log("articles", articles);
+  // console.log("articles", articles);
   return (
-    <>
+    <div className="w-[1216px] flex justify-center m-auto">
       <div className="grid grid-cols-3 gap-5">
         {articles.map((article) => {
           return (
-            <div className="border border-solid rounded-xl p-4 w-[392px]">
-              <div className="mb-4">
-                <img
-                  src={
-                    article.cover_image
-                      ? article.cover_image
-                      : "https://picsum.photos/536/354"
-                  }
-                  alt=""
-                  className="h-[240px] w-[360px] rounded-md"
-                />
-              </div>
-              <div className="h-[188px] p-2 flex justify-between flex-col">
-                <p className="flex flex-wrap">
-                  {article.tag_list.map((tag) => {
-                    return (
-                      <label className="text-sm mr-1 mb-1 rounded-md text-[#4B6BFB] bg-[#4b6bfb0d] px-2.5 py-1">
-                        {tag}
-                      </label>
-                    );
-                  })}
-                </p>
-                <p className="font-semibold text-2xl line-clamp-3">
-                  {article.title}
-                </p>
-                <div className="flex items-center text-[#97989F] text-base">
+            <div>
+              <div className="border border-solid rounded-xl p-4 w-[392px]">
+                <div className="mb-4">
                   <img
-                    className="h-9 w-9 rounded-full mr-3"
-                    src={article.user.profile_image}
+                    src={
+                      article.cover_image
+                        ? article.cover_image
+                        : "https://picsum.photos/536/354"
+                    }
                     alt=""
+                    className="h-[240px] w-[360px] rounded-md"
                   />
-                  {article.user.name}
-                  <div className="ml-5">{article.readable_publish_date}</div>
+                </div>
+                <div className="h-[188px] p-2 flex justify-between flex-col">
+                  <p className="flex flex-wrap">
+                    {article.tag_list.map((tag) => {
+                      return (
+                        <label className="text-sm mr-1 mb-1 rounded-md text-[#4B6BFB] bg-[#4b6bfb0d] px-2.5 py-1">
+                          {tag}
+                        </label>
+                      );
+                    })}
+                  </p>
+                  <p className="font-semibold text-2xl line-clamp-3">
+                    {article.title}
+                  </p>
+                  <div className="flex items-center text-[#97989F] text-base">
+                    <img
+                      className="h-9 w-9 rounded-full mr-3"
+                      src={article.user.profile_image}
+                      alt=""
+                    />
+                    {article.user.name}
+                    <div className="ml-5">{article.readable_publish_date}</div>
+                  </div>
                 </div>
               </div>
             </div>
           );
         })}
       </div>
-    </>
+    </div>
   );
 };
